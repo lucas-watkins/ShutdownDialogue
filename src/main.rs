557a0579a1e3899ae4ff25a2f3d6221524c28949
lucas-watkins <lucas_a_watkins@protonmail.com>
2025-01-05 20:30:08 -0500

    Partially Port to Rust

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+mod config;
+
+use crate::config::{ShutdownCommands};
+use eframe::egui::{Button, Context, CentralPanel, ViewportBuilder};
+use eframe::{App, Frame};
+use std::process::Command;
+
+#[derive(Default)]
+struct Window {
+    shutdown_commands: ShutdownCommands,
+}
+
+impl Window {
+    fn exec_command(&self, command: &String) {
+        let result = if cfg!(target_os = "windows") {
+            Command::new("cmd").args(["/C", command]).spawn()
+        } else {
+            Command::new("sh").args(["-c", command]).spawn()
+        };
+        result.expect("Error executing command").wait().unwrap();
+    }
+}
+
+impl App for Window {
+    fn update(&mut self, ctx: &Context, _: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Shutdown Options:");
+
+            let shutdown = Button::new("Shutdown");
+            let sleep = Button::new("Sleep");
+            let lock = Button::new("Lock");
+            let logoff = Button::new("Logoff");
+            let restart = Button::new("Restart");
+
+            ui.add_space(10.);
+
+            ui.horizontal(|ui| {
+                if ui.add_sized([75., 25.], lock).clicked() {
+                    self.exec_command(&self.shutdown_commands.lock)
+                };
+                if ui.add_sized([75., 25.], logoff).clicked() {
+                    self.exec_command(&self.shutdown_commands.logoff)
+                };
+                if ui.add_sized([75., 25.], sleep).clicked() {
+                    self.exec_command(&self.shutdown_commands.sleep)
+                };
+                if ui.add_sized([75., 25.], restart).clicked() {
+                    self.exec_command(&self.shutdown_commands.restart)
+                };
+                if ui.add_sized([75., 25.], shutdown).clicked() {
+                    self.exec_command(&self.shutdown_commands.shutdown)
+                }
+            })
+        });
+    }
+}
+
+fn main() {
+    eframe::run_native(
+        "Shutdown Dialogue",
+        eframe::NativeOptions {
+            viewport: ViewportBuilder::default()
+                .with_inner_size([425., 100.])
+                .with_resizable(false),
+            vsync: true,
+
+            ..Default::default()
+        },
+        Box::new(|_| {
+            Ok(Box::<Window>::new(Window {
+                shutdown_commands: ShutdownCommands::default(),
+            }))
+        }),
+    )
+    .expect("Window failed to start");
+}
